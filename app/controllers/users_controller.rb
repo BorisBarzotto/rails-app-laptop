c4fb5b5e61cf948a95d4db06cb542466c1d2e181
BorisBarzotto <fakieboris@gmail.com>
2022-09-24 16:33:10 -0400

    Authentication helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:show, :edit, :update]
+
     def show
-      @user = User.find(params[:id])
+      
       @pagy, @user_articles = pagy(@user.articles)
       
     end
@@ -15,11 +17,11 @@ class UsersController < ApplicationController
     end
 
     def edit
-      @user = User.find(params[:id])
+      
     end
 
     def update
-      @user = User.find(params[:id])
+      
       if @user.update(user_params)
         flash[:notice] = "User edited"    
         redirect_to user_path(@user[:id])  
@@ -32,6 +34,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+          session[:user_id] = @user.id
           flash[:notice] = "Welcome #{@user.username} "
           redirect_to articles_path
         else
@@ -44,5 +47,9 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username, :email, :password) 
     end
+
+    def set_user
+      @user = User.find(params[:id])
+    end  
 
 end
